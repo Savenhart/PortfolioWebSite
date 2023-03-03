3ab4c36673a93d08944aad1e28d6666ec6c0db1c
Theo Lounissi <Theo.lounissi@gmail.com>
2023-03-03 04:51:20 +0100

    Enable cors on backend

--- portfolio-api/src/main.ts
+++ portfolio-api/src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res) => {
-    res.set({
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Headers':
-        "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-    });
-  });
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(80);
 }
 bootstrap();
