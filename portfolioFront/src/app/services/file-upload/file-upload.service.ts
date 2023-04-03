8ca4b5a3b6f8c593a52edc7672a490b8756f0266
Savenhart <theo.lounissi@gmail.com>
2023-04-03 12:05:20 +0200

    Typo
    Typo correction

--- portfolioFront/src/app/services/file-upload/file-upload.service.ts
+++ portfolioFront/src/app/services/file-upload/file-upload.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environnements/environnement';
+import { environment } from 'src/environment/environment';
 
 @Injectable({
   providedIn: 'root',
